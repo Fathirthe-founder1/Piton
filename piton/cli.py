a684ade69a7613fbeaa1d2899264ad373aa5f478
Jameson Greogory Void <fathiragata22@gmail.com>
2026-07-15 01:02:56 +0700

    Update cli.py

--- piton/cli.py
+++ piton/cli.py
@@ -1,11 +1,11 @@
 import sys
 from pathlib import Path
-from .core import translate, run_piton
+from .core import translate, run_pitonx
 
 def main():
     if len(sys.argv) < 2:
-        print("Piton CLI")
-        print("Cara pakai: piton nama_file.pt")
+        print("PitonX CLI")
+        print("Cara pakai: pitonx nama_file.pt")
         return
     file_path = sys.argv[1]
     if not Path(file_path).exists():
@@ -14,6 +14,6 @@ def main():
     with open(file_path, 'r', encoding='utf-8') as f:
         code = f.read()
     try:
-        run_piton(code)
+        run_pitonx(code)
     except Exception as e:
         print(f"ERROR: {e}")

781e08e5694c866362c384c661cedc6d0051d981
Jameson Greogory Void <fathiragata22@gmail.com>
2026-07-15 15:46:53 +0700

    Downgrade version from 1.0.5 to 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pitonx',
-    version='1.0.5',
+    version='1.0.1',
     packages=find_packages(),
     entry_points={
         'console_scripts': [

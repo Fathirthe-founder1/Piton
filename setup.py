a7f379b8ce15f05428fa0a66359c92d8142b7660
Jameson Greogory Void <fathiragata22@gmail.com>
2026-07-22 14:45:59 +0700

    Bump version from 8.1.0 to 8.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pitonx',
-    version='8.1.0',
+    version='8.1.1',
     packages=find_packages(),
     entry_points={'console_scripts': ['pitonx = pitonx.repl:main']},
     description='Bahasa Pemrograman Indonesia berbasis Python',
